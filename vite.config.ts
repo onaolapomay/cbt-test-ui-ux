055dc85906612b90b846ecbf9e94ca0fd72d7f40
onaolapomay <raphy.mayo@gmail.com>
2026-09-22 14:31:32 +0100

    feat: add netlify tanstack start plugin for correct SSR routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 import viteReact from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     tanstackStart(),
+    netlify(),
     viteReact(),
   ],
 })
